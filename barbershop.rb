54ab1bbf2a9690fad175c84b69ebeffc147c37d3
Vladimir Chernov <v.chernov@zebrains.team>
2022-09-08 13:57:46 +0300

    1. Created functionality for adding user data to a file. 2. Added .gitignore.

--- barbershop.rb
+++ barbershop.rb
@@ -12,5 +12,9 @@ post '/' do
 	@title = 'Thank you!'
 	@message = "Dear #{@user_name} we'll be waiting for you at #{@date_time}"
 
+	notebook = File.open 'notebook.txt', 'a'
+	notebook.write "User: #{@user_name}, Phone: #{@phone}, Date and time: #{@date_time}"
+	notebook.close
+
 	erb :message
 end
